7bdb13a7a22b4301d321840f0c75b164c2cb089e
Алексей Шиян <alex-shiyan@yandex.ru>
2022-02-03 16:58:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//
+// Домашнее задание к занятию 11. Структуры.
+//
+// Задача 1. Разработать структуру для решения линейного уравнения 0=kx+b. 
+// Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+// Для решения уравнения предусмотреть метод Root. 
+// Создать экземпляр разработанной структуры. Осуществить использование экземпляра в программе.
+//
+
+using DZ11;
+// Задаем коэффициент уравнеия k
+double k;
+Console.Write("Введите коэффициент k= ");
+while (!double.TryParse(Console.ReadLine(), out k))
+{
+    Console.WriteLine("Ошибка ввода! Введите целое число");
+}
+// Задаем коэффициент уравнеия b
+double b;
+Console.Write("Введите коэффициент b= ");
+while (!double.TryParse(Console.ReadLine(), out b))
+{
+    Console.WriteLine("Ошибка ввода! Введите целое число");
+}
+Console.WriteLine();
+// Создаем экзепляр uravnenie струтуры Uravnenie
+Uravnenie uravnenie = new Uravnenie(k, b);
+// Вызываем метод Root и выводим строку на экран 
+Console.WriteLine(uravnenie.Root());
+Console.ReadKey();
---
+++ Uravnenie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ11
+{
+    internal struct Uravnenie
+    {
+        double k;
+        double b;
+
+        public Uravnenie(double k, double b)
+        {
+            this.k = k; 
+            this.b = b;
+        }
+            public string Root ()
+        {
+            if (k == 0)
+                return "Решения нет";
+            
+            double x = (-1) * (b / k);
+            return $"Ответ: неизвестное Х={x}";
+        }
+
+    }
+}
+    
